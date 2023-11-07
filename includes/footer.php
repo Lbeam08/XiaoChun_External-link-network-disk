6153a1a3ea21f6c45a006987dc181312d2942198
unknown <lbeam0326@gmail.com>
2023-11-07 19:51:19 +0800

    add 2 projects

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer text-center">
+      <div class="container">
+        <p class="text-muted" style='color: #000'>Copyright &copy; <?php echo date('Y')?> <a href="/" style='color: #ff94c2'><?php echo $conf['title']?></a> <?php echo $conf['tongji']?> </p>
+      </div>
+    </footer>
+<script src="//cdn.staticfile.org/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="//cdn.staticfile.org/bootstrap-material-design/0.5.10/js/material.min.js"></script>
+<script src="//cdn.staticfile.org/bootstrap-material-design/0.5.10/js/ripples.min.js"></script>
+<script>
+  $.material.init();
+</script>
+
+<!-- 尾端 -->
